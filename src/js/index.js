5b99843a223d3d9eb472925ec706b0c75cd8c2b6
wenciu <72387149+wenciu@users.noreply.github.com>
2024-03-14 17:30:54 +0100

    .

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import items from "./items.js";
+
+console.log(items);
+
+// function diplayMenuItems(menuItems) {
+//   let displayMenu = menuItems.map(function (item) {
+//     // console.log(item);
