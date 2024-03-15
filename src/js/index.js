f30c324786afa26520915c5e4cc7776cba6074ab
wenciu <72387149+wenciu@users.noreply.github.com>
2024-03-15 11:46:10 +0100

    .

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,39 @@
-import items from "./items.js";
+document.addEventListener("DOMContentLoaded", function () {
+  const cardContainer = document.querySelector(".cards-container");
+  fetch("https://json.extendsclass.com/bin/33c955123b3f")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      let singleCard = "";
 
-console.log(items);
+      data.forEach((item) => {
+        singleCard += `
+        <article class="single-card">
+        <div class="card-left">
+          <img src=${item.img} alt="${item.name}">
+        </div>
+        <div class="card-right">
+          <h3 class="name">${item.name}</h3>
+          <h3 class="price">$${item.price}</h3>
+          <p class="description">${item.desc}
+          </p>
+          <div class="tags-container">
+            <div class="tag">${item.tags[0]}</div>
+            <div class="tag">${item.tags[1]}</div>
+            <div class="tag">${item.tags[2]}</div>
+          </div>
+        </div>
+      </article>`;
+      });
 
-// function diplayMenuItems(menuItems) {
-//   let displayMenu = menuItems.map(function (item) {
-//     // console.log(item);
+      cardContainer.innerHTML = singleCard;
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      dataContainer.textContent = "Failed to fetch data";
+    });
+});
